83f9e109f610b244b522ca79f69fbda87853c241
Yao <sukoyao1101@gmail>
2019-04-19 18:46:07 +0800

    Add logout feature

--- routes/user.js
+++ routes/user.js
@@ -53,7 +53,8 @@ router.post('/register', (req, res) => {
 
 // 登出
 router.get('/logout', (req, res) => {
-  res.render('logout')
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
